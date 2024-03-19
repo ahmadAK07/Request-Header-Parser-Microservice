a2c1f1bac8bb6aff645dbff017bdf96a38a16801
ahmadAK07 <156776092+ahmadAK07@users.noreply.github.com>
2024-03-19 16:50:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/whoami', function (req, res) {
+  res.json({ 
+    ipaddress: req.socket.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
 });
-
 
 
 // Listen on port set in environment variable or default to 3000
